6aa139c72b846eba63dd3cec937dd6f0a559bb23
mandamnyc <amandajmunch@gmail.com>
2017-01-16 16:32:32 -0500

    added poup

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,3 +3,11 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "king";
 var cardFour = "queen";
+if (cardOne === cardFour) {
+	alert("You found a match!")
+} else if (cardTwo === cardThree)
+	alert("You found a match!")
+	else {
+		alert("Sorry, try again."):
+	}
+	
