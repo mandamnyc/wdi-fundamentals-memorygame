8b25792373c8b709a786673827cf25c79a4b121f
Amanda Munch <amandajmunch@gmail.com>
2017-01-17 21:48:15 -0500

    added cards

--- main.js
+++ main.js
@@ -3,11 +3,25 @@ var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "king";
 var cardFour = "queen";
-if (cardOne === cardFour) {
+
+var board = document.getElementById('game-board');
+
+ for (var i=0; i<cards.length; i++) {
+var cardElement = document.createElement('div');
+
+cardElement.className = 'card';
+ board.appendChild(cardElement);
+  }
+
+}
+
+/* if (cardOne === cardFour) {
 	alert("You found a match!")
 } else if (cardTwo === cardThree)
 	alert("You found a match!")
 	else {
 		alert("Sorry, try again."):
-	}
+	} */
+
+
 	
